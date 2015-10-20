997a65fce04ddcb4da94cd64d47c702a74b56ce8
Sean <sean.feeney@quinnipiac.edu>
2015-10-20 03:37:23 +0000

    Add user authorization

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
@@ -38,10 +39,22 @@ class QuestionUpdateView(UpdateView):
     template_name = 'question/question_form.html'
     fields = ['title', 'description']
 
+    def get_object(self, *args, **kwargs):
+        object = super(QuestionUpdateView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
+
 class QuestionDeleteView(DeleteView):
     model = Question
     template_name = 'question/question_confirm_delete.html'
     success_url = reverse_lazy('question_list')
+
+    def get_object(self, *args, **kwargs):
+        object = super(QuestionDeleteView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
 
 class AnswerCreateView(CreateView):
     model = Answer
@@ -64,6 +77,8 @@ class AnswerUpdateView(UpdateView):
 
     def get_success_url(self):
         return self.object.question.get_absolute_url()
+
+
 
 class AnswerDeleteView(DeleteView):
     model = Answer
